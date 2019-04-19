9234cd14914786e499e2fd78bec6aa027786294b
Lars Norberg <gitkraken@larsnorbergofficial.com>
2019-04-19 19:47:38 +0200

    PKGBUILD for custom linuxconsole

---
+++ ArchPackages/linuxconsoletools/inputattach.conf.d
@@ -0,0 +1,9 @@
+#
+# Configuration for inputattach
+#
+# IAPARAMS is an array of inputattach arguments, see 'inputattach --help'.
+# An inputattach instance will be started for each element.
+
+IAPARAMS=(
+  "--wacom_iv /dev/ttyS0"
+)
